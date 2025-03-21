2993543f6fbb96509704ccbfe3029c8cfafe1245
Nelly Simkova <sk8high3r@ya.ru>
2025-03-21 12:38:16 +0300

    Update relations between models and add validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :journal
+
+  has_many :article_authors
+  has_many :authors, through: :article_authors
+
+  validates :orig_name, :eng_name, :publish_date, :doi, presence: true
+  validates :doi, presence: true, uniqueness: true, format: { with: /\A10\.\d{4,9}\/[-._;()\/:A-Z0-9]+\z/i }
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,7 @@
 class Author < ApplicationRecord
   validates :fullname, presence: true
   validates :orcid, presence: true, format: { with: /\A\d{4}-\d{4}-\d{4}-\d{3}[0-9X]\z/ }
+
+  has_many :article_authors
+  has_many :articles, through: :article_authors
 end
