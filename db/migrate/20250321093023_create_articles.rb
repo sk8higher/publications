c5152c5ca1b6d068979c599eff136dc413ed0462
Nelly Simkova <sk8high3r@ya.ru>
2025-03-21 12:31:30 +0300

    Add Article model

---
+++ db/migrate/20250321093023_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[7.1]
+  def change
+    create_table :articles do |t|
+      t.string :orig_name
+      t.string :eng_name
+      t.date :publish_date
+      t.string :doi
+      t.belongs_to :journal, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
