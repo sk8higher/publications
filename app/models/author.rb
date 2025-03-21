b8be4818e2d81873158ebd33dca1c1cf209c9386
Nelly Simkova <sk8high3r@ya.ru>
2025-03-21 15:33:51 +0300

    Add uniqueness to ocrid

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ApplicationRecord
   validates :fullname, presence: true
-  validates :orcid, presence: true, format: { with: /\A\d{4}-\d{4}-\d{4}-\d{3}[0-9X]\z/ }
+  validates :orcid, presence: true, uniqueness: true, format: { with: /\A\d{4}-\d{4}-\d{4}-\d{3}[0-9X]\z/ }
 
   has_many :article_authors
   has_many :articles, through: :article_authors
